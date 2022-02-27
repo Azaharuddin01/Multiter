167e4320c9ee9bd3e47f1df7c9de460ba403f8e3
user <user@DESKTOP-3G4HHUT>
2022-02-27 09:28:50 -0800

    Add project files.

---
+++ Multiter/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using BusinssAccessLayer;
+using CommanLayer.Models;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Multiter.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multiter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BLEmployeeBusiness busness = new BLEmployeeBusiness();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var emp = busness.GetEmployees();
+            return View(emp);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employees employee)
+        {
+            var result = busness.CreateEmployee(employee);
+            if (result)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("error", "Error in create employee !");
+                return View(employee);
+            }
+
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
